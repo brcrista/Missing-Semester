099e3ad9e850ba3581b78e132f55bed7d1ee66c7
Brian Cristante <brian.cristante@gmail.com>
2020-04-30 20:53:22 -0400

    Touch up Lecture 4 Exercise 2

--- lecture4/exercise2.sh
+++ lecture4/exercise2.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
-# Find the number of words in /usr/share/dict words that contain at least three a's and don't have an 's ending.
-cat /usr/share/dict/words \
+echo "Find the number of words in /usr/share/dict/words that contain at least three a's and don't have an 's ending."
+three_as=$(cat /usr/share/dict/words \
 | tr "[:upper:]" "[:lower:]" \
-| egrep ".*a.*a.*a.*[^'][^s]$" \
+| grep -E ".*a.*a.*a.*[^'][^s]$")
+
+echo "$three_as" | wc -l
+
+echo
+echo "What are the three most common last two letters of those words?"
+echo "$three_as" \
 | sed -E "s/^.*(..)$/\1/g" \
 | sort \
 | uniq -c \
 | sort -nr \
 | head -n 3
+
+echo
+echo "How many of those two-letter combinations are there?"
+# Assume Latin alphabet (no loan words, such as fiancée)
+echo {a..z}{a..z} | wc -w # == 26 * 26
